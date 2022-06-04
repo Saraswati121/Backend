e6b1d17aa3c5e671d5ad2d3820ddc7b3ffd9b1ef
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-06-04 14:00:31 +0530

    commit

--- day-3/TODO/index.js
+++ day-3/TODO/index.js
@@ -6,12 +6,14 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.get("/",(req,res)=>{
-    
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+            res.end(data)
+    })
 })
 
 app.post("/",(req,res)=>{
-    fs.readFile("./db.json","utf-8", (err,data)=>{
-        const parsed=JSON.parsed(data)
+    fs.readFile("./db.json",{encoding:"utf-8"}, (err,data)=>{
+        const parsed=JSON.parse(data)
         parsed.todos=[...parsed.todos,req.body];
 
         fs.writeFile("./db.json",JSON.stringify(parsed),"utf-8",()=>{
@@ -25,8 +27,16 @@ app.put("/",(req,res)=>{
 
 })
 
-app.delete("/",(req,res)=>{
+app.delete("/:id",(req,res)=>{
+    const {id} =req.params
+    fs.readFile("./db.json", {encoding:"utf-8"},(err,data)=>{
+        const parsed=JSON.parse(data)
+        parsed.todos=parsed.todos.filter(el => el.id !== id);
 
+        fs.writeFile("./db.json", JSON.stringify(parsed),"utf-8",()=>{
+            res.end("todo deleted")
+        })
+    })
 })
 
 app.listen(8080,()=>{
