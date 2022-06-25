ecf6738347c9f755059f16abfeb07acd60a1cba6
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-06-25 20:51:54 +0530

    commit

---
+++ NEM/day-3/Todo/index.js
@@ -0,0 +1,46 @@
+const express=require("express")
+const fs=require("fs")
+const { request } = require("http")
+
+const app=express()
+
+app.use(express.urlencoded({encoding:true}))
+app.use(express.json())
+
+app.post("/",(req,res)=>{
+    fs.readFile("./db.json",{encoding:"utf-8"},(err,data)=>{
+        const parsed=JSON.parse(data)
+        parsed.list=[...parsed.list,req.body];
+
+        fs.writeFile("./db.json",JSON.stringify(parsed),"utf-8",()=>{
+            res.send("Todo list created")
+        })
+    })
+})
+
+app.get("/",(req,res)=>{
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        res.send(data)
+    })
+})
+
+app.delete("/:id",(req,res)=>{
+    const {id}= req.params
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        const parsed=JSON.parse(data)
+        parsed.list=parsed.list.filter(el => el.id != id)
+
+        fs.writeFile("./db.json",JSON.stringify(parsed),"utf-8",()=>{
+            res.end("list deleted")
+        })
+    })
+})
+
+app.put("/",(req,res)=>{
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        
+    })
+})
+app.listen(8080,()=>{
+    console.log("server started")
+})
